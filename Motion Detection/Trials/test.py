f8a54a0759b636c679dcb0f7274bd6aaf15f1a29
Gellesh <khaloudfire104@gmail.com>
2021-07-08 18:40:52 +0200

    add blur and change image to grey

--- Motion Detection/Trials/test.py
+++ Motion Detection/Trials/test.py
@@ -12,13 +12,17 @@ cap = cv2.VideoCapture(0)
 while(1):
     # read frames
     ret, img = cap.read()
+
+    #Gray conversion and noise reduction (smoothening)
+    gray_frame=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gray_frame=cv2.GaussianBlur(gray_frame,(25,25),0)
       
     # apply mask for background subtraction
-    fgmask1 = fgbg1.apply(img)
-    fgmask2 = fgbg2.apply(img)
-    fgmask3 = fgbg3.apply(img)
+    fgmask1 = fgbg1.apply(gray_frame)
+    fgmask2 = fgbg2.apply(gray_frame)
+    fgmask3 = fgbg3.apply(gray_frame)
       
-    cv2.imshow('Original', img)
+    cv2.imshow('Original', gray_frame)
     cv2.imshow('KNN', fgmask1)
     cv2.imshow('MOG2', fgmask2)
     cv2.imshow('GMG', fgmask3)
