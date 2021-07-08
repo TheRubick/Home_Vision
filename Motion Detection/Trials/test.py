4f795aae3a38d1c2d2b56afdd49ef0a44913ffac
therubick <mohamed_ayman_07@hotmail.com>
2021-07-08 18:44:36 +0200

    testing the functions

--- Motion Detection/Trials/test.py
+++ Motion Detection/Trials/test.py
@@ -3,10 +3,10 @@ import numpy as np
 import cv2
   
 # creating object
-fgbg1 = cv2.createBackgroundSubtractorKNN(detectShadows=False)
-fgbg2 = cv2.createBackgroundSubtractorMOG2(history=120,detectShadows=False)
-fgbg3 = cv2.bgsegm.createBackgroundSubtractorGMG(initializationFrames=120,decisionThreshold=0.8)
-fgbg4 = cv2.bgsegm.createBackgroundSubtractorMOG(history=120,noiseSigma=20.6)
+#fgbg1 = cv2.createBackgroundSubtractorKNN(detectShadows=False)
+fgbg2 = cv2.createBackgroundSubtractorMOG2(history=500,detectShadows=False)
+#fgbg3 = cv2.bgsegm.createBackgroundSubtractorGMG(initializationFrames=120,decisionThreshold=0.8)
+fgbg4 = cv2.bgsegm.createBackgroundSubtractorMOG(history=500)
 
 # capture frames from a camera 
 cap = cv2.VideoCapture(0)
@@ -15,16 +15,16 @@ while(1):
     ret, img = cap.read()
       
     # apply mask for background subtraction
-    fgmask1 = fgbg1.apply(img)
+    #fgmask1 = fgbg1.apply(img)
     fgmask2 = fgbg2.apply(img)
-    fgmask3 = fgbg3.apply(img)
+    #fgmask3 = fgbg3.apply(img)
     fgmask4 = fgbg4.apply(img)
     
       
     cv2.imshow('Original', img)
-    cv2.imshow('KNN', fgmask1)
+    #cv2.imshow('KNN', fgmask1)
     cv2.imshow('MOG2', fgmask2)
-    cv2.imshow('GMG', fgmask3)
+    #cv2.imshow('GMG', fgmask3)
     cv2.imshow('MOG', fgmask4)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
