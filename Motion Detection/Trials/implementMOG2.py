125e1a0a9bdad1fa34b3b1811bf0d577ecbf3691
therubick <mohamed_ayman_07@hotmail.com>
2021-07-11 00:02:43 +0200

    adding motion detection benchmark

---
+++ Motion Detection/Trials/implementMOG2.py
@@ -0,0 +1,6 @@
+'''
+steps of the algorithm:
+
+1- set the number of guassian mixture models and the other constants: Cthr, mahalanabolis distance, Cf, backgroundPortion
+2- start with the first sample 
+'''
