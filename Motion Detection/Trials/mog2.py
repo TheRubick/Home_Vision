702020a6b3d21e46b72cf9641d1cde205ea0f004
Gellesh <khaloudfire104@gmail.com>
2021-07-14 17:41:18 +0200

    add change to mog2.py

--- Motion Detection/Trials/mog2.py
+++ Motion Detection/Trials/mog2.py
@@ -6,18 +6,23 @@ import cv2
 def skipFrame(frames, cap):
     totalFrames = getFPS(cap)
     skip = totalFrames // frames    # 30 / 3 = 10  1 ..1 ..11 ..21 ..22 .. 30  ***  2    
+    return skip
 
 def getFPS(cap):
     return cap.get(cv2.CAP_PROP_FPS)
 
-def getNonZeroCount(frame,threshold = 1000):
+def getNonZeroCount(frame,threshold = 10000):
     nonZeroCount = np.count_nonzero(frame)
-    return (threshold > nonZeroCount)
+    threshold = cap.get(3) * cap.get(4) / 100
+   
+    return (nonZeroCount > threshold)
+    
 
-def frameVote(frameChecker):
-    return (np.count_nonzero(frameChecker) > 3)
+def frameVote(frameChecker, frameNumber):
+    return (np.count_nonzero(frameChecker) > frameNumber //2 )
 
-frameChecker = np.zeros(6)
+frameNumber = 9
+frameChecker = np.zeros(frameNumber)
 frameIdx = 0
 textColor = (255, 255, 255)
 textPosition = (100, 100)
@@ -34,32 +39,38 @@ count = 0
 
 while(1):
     # read frames
-    
-    if  count % 10 == 0 :
-        
-        ret, img = cap.read()
+    ret, img = cap.read()
 
-        #Gray conversion and noise reduction (smoothening)
-        gray_frame=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        gray_frame=cv2.GaussianBlur(gray_frame,(25,25),0)
+    
+   
+        
+    #Gray conversion and noise reduction (smoothening)
+    gray_frame=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gray_frame=cv2.GaussianBlur(gray_frame,(25,25),0)
         
     
-        fgmask = fgbg.apply(gray_frame)
+    fgmask = fgbg.apply(gray_frame)
+
+
+    if  count % 3 == 0 :
  
-        frameChecker[frameIdx] = getNonZeroCount(fgmask)
+        frameChecker[frameIdx] = getNonZeroCount(fgmask,cap)
 
-        if(frameVote(frameChecker)):
+        if(frameVote(frameChecker, frameNumber)):
             cv2.putText(fgmask, 'Someones stealing your honey', textPosition
             , cv2.FONT_HERSHEY_SIMPLEX, .5, textColor, 2, cv2.LINE_AA)
     
         frameIdx += 1
-        frameIdx %= 6
-        cv2.imshow('Original', img)
-        cv2.imshow('MOG2', fgmask)
+        frameIdx %= frameNumber
+
+        print(frameChecker)
+
 
 
     count += 1
     count %= fps-1
+    cv2.imshow('Original', img)
+    cv2.imshow('MOG2', fgmask)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
