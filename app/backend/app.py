1e81cfc80a04d1efb898e146da03c5b8e1419d70
yousefdawood <yocomohamed@yahoo.com>
2021-07-19 00:44:48 +0200

    Add faces and settings components

--- app/backend/app.py
+++ app/backend/app.py
@@ -11,7 +11,7 @@ CORS(app, support_credentials=True)
 
 
 def gen_frames():
-    camera = cv2.VideoCapture(1)  
+    camera = cv2.VideoCapture(0)  
     while feed:
         success, frame = camera.read()  # read the camera frame
         if not success:
@@ -42,3 +42,123 @@ def video_feed():
     global feed 
     feed = True
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+person_faces = []
+person_name=''
+@app.route('/take_photo',methods=['GET'])
+def take_photo():
+    print("in take photo")
+    camera = cv2.VideoCapture(0)
+    success, frame = camera.read()  # read the camera frame
+    camera.release()
+    global person_faces
+    person_faces.append(frame)
+    ret, buffer = cv2.imencode('.jpg', frame)
+    frame = buffer.tobytes()
+    var = b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
+    print("finish take photo")
+    return Response(var, mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/take_photo2',methods=['GET'])
+def take_photo2():
+    print("in take photo")
+    camera = cv2.VideoCapture(0)
+    success, frame = camera.read()  # read the camera frame
+    camera.release()
+    global person_faces
+    person_faces.append(frame)
+    ret, buffer = cv2.imencode('.jpg', frame)
+    frame = buffer.tobytes()
+    var = b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
+    print("finish take photo")
+    return Response(var, mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/take_photo3',methods=['GET'])
+def take_photo3():
+    print("in take photo")
+    camera = cv2.VideoCapture(0)
+    success, frame = camera.read()  # read the camera frame
+    camera.release()
+    global person_faces
+    person_faces.append(frame)
+    ret, buffer = cv2.imencode('.jpg', frame)
+    frame = buffer.tobytes()
+    var = b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
+    print("finish take photo")
+    return Response(var, mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/take_photo4',methods=['GET'])
+def take_photo4():
+    print("in take photo")
+    camera = cv2.VideoCapture(0)
+    success, frame = camera.read()  # read the camera frame
+    camera.release()
+    global person_faces
+    person_faces.append(frame)
+    ret, buffer = cv2.imencode('.jpg', frame)
+    frame = buffer.tobytes()
+    var = b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
+    print("finish take photo")
+    return Response(var, mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/take_photo5',methods=['GET'])
+def take_photo5():
+    print("in take photo")
+    camera = cv2.VideoCapture(0)
+    success, frame = camera.read()  # read the camera frame
+    camera.release()
+    global person_faces
+    person_faces.append(frame)
+    ret, buffer = cv2.imencode('.jpg', frame)
+    frame = buffer.tobytes()
+    var = b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
+    print("finish take photo")
+    return Response(var, mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/take_photo6',methods=['GET'])
+def take_photo6():
+    print("in take photo")
+    camera = cv2.VideoCapture(0)
+    success, frame = camera.read()  # read the camera frame
+    camera.release()
+    global person_faces
+    person_faces.append(frame)
+    ret, buffer = cv2.imencode('.jpg', frame)
+    frame = buffer.tobytes()
+    var = b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
+    print(person_faces[0])
+    return Response(var, mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/cancel_faces',methods=['GET'])
+def cancel_faces():
+    global person_faces
+    global person_name
+    person_faces=[]
+    person_name=''
+    res=""
+    print("caaancceeeellll")
+    return Response(res)
+
+@app.route('/save_name',methods=['POST'])
+def save_name():
+    global person_name
+    json = request.get_json()
+    person_name=json.get('Name')
+    print(person_name)
+    res=""
+    return jsonify(res)
+
+settings=[True, False]
+@app.route('/current_settings',methods=['GET'])
+def current_settings():
+    return jsonify(settings)
+
+@app.route('/update_settings',methods=['POST'])
+def update_settings():
+    global settings
+    json = request.get_json()
+    settings=json.get('Current_Settings')
+    print(settings)
+    res=""
+    return jsonify(res)
--- app/src/views/Settings.vue
+++ app/src/views/Settings.vue
@@ -10,16 +10,37 @@
         <ion-title >Settings </ion-title>
       </ion-toolbar>
     </ion-header>
-    
     <ion-content>
-        
-    </ion-content>
+      <ion-list>
+        <ion-item>
+          <ion-label>Face Recognition</ion-label>
+          <ion-toggle
+            v-model="current_settings[0]"
+            @ionChange="face_toggled"
+            :checked="current_settings[0]">
+          </ion-toggle>
+        </ion-item>
+
+          <ion-item>
+          <ion-label>Motion Detection</ion-label>
+          <ion-toggle
+            v-model="current_settings[1]"
+            @ionChange="motion_toggeled"
+            :checked="current_settings[1]">
+          </ion-toggle>
+        </ion-item>
+
+        </ion-list>
+      </ion-content>
   </ion-page>
 </template>
 <script>
 import {IonButtons, IonBackButton, IonContent, IonHeader, IonPage, IonTitle,
-     IonToolbar} from '@ionic/vue';
+     IonToolbar,IonLabel, IonList, IonItem, IonToggle} from '@ionic/vue';
 
+import axios from 'axios';
+const currentSettingsPath = "/current_settings";
+const updateSettingsPath = "/update_settings";
 
 export default {
   name: 'Settings',
@@ -30,7 +51,39 @@ export default {
     IonTitle,
     IonToolbar,
     IonButtons,
-    IonBackButton
+    IonBackButton,
+    IonLabel,
+    IonList,
+    IonItem,
+    IonToggle
+  },
+  data(){
+    return{
+      current_settings:[]
+    }
+  },
+  methods:{
+    face_toggled(){
+      var payload = {
+            Current_Settings:this.current_settings
+      }
+      axios.post(this.$hostName+updateSettingsPath, payload)
+      .catch(err => console.log(err))
+    },
+    motion_toggeled(){
+      var payload = {
+            Current_Settings:this.current_settings
+      }
+      axios.post(this.$hostName+updateSettingsPath, payload)
+      .catch(err => console.log(err))
+    }
+  },
+  beforeMount(){
+      axios.get(this.$hostName+currentSettingsPath)
+      .then(res => {this.current_settings[0]=res.data[0];
+          this.current_settings[1]=res.data[1];
+          })
+      .catch(err => console.log(err))
   }
 }
 </script>
